abc00a8600c8bac03110c68c2c1c74eebeedd3ef
Jam52 <jamie.sajdak@gmail.com>
2020-11-13 14:49:17 -0500

    Update About me page

--- src/Containers/AboutPage/AboutPage.js
+++ src/Containers/AboutPage/AboutPage.js
@@ -24,30 +24,29 @@ const AboutPage = () => {
             <img src={profilePic} alt="jamie photograph"></img>
           </div>
           <p className={classes.landing_para}>
-            Hi, thank you for checking out my portfolio site! I am an aspiring
+            "Hi, thank you for checking out my portfolio site! I am an aspiring
             web developer, formerly industrial designer, with a passion for
             building things. Please scroll down for a little more about me and
-            my journey so far.
+            my journey so far."
           </p>
         </div>
       </div>
 
-      <div className={[classes.section, classes.section___dark].join(' ')}>
+      <div className={classes.section}>
         <div className={classes.section_paragraph}>
           <h3 className={classes.section_subtitle}>Begin at the begining</h3>
 
           <p>
             Ive always been a builder, one of those kids obsessed with legos,
             making sand castles and scultpures out of mud and sticks (I still
-            do) and the drive to build and create never left me.
+            do) and the drive to build and create has never left me.
           </p>
           <p>
-            My path to becoming a web developer is roundabout as I started with
-            a 9 year career as an industrial designer. After a BA degree in
-            furniture and product design, that part of my story is long and
-            contains all kinds of things like moving to Canada and starting a
-            furniture company with my father. It wasnt until I was around 30
-            that my coding journey started.
+            My first career was in industrial design, that part of my story is
+            long and contains all kinds of things including a BA in furniture
+            and industrial design, emigrating to Canada and starting a furniture
+            business with my father. It wasn’t until I was around 30 that my
+            coding journey started.
           </p>
         </div>
         <div className={classes.section_images}>
@@ -58,25 +57,28 @@ const AboutPage = () => {
       <div className={classes.section}>
         <div className={classes.section_paragraph}>
           <h3 className={classes.section_subtitle}>My road to Code</h3>
-          <p>Sorry about the cheesy header, I couldnt resist!</p>
+          <p>Sorry about the cheesy header, I couldn't resist!</p>
           <p>
             A friend of mine introduced me to the world of code via a few basic
             tutorials, when he was going through a bootcamp, he knew me well and
             thought that I might enjoy it.
           </p>
           <p>
-            Turns out he was right, I got hooked. For a few years I hobbie coded
-            doing a lot of intro courses in several areas and languages
+            Turns out he was right, I became hooked. For a few years I hobby
+            coded doing a lot of intro courses in several areas and languages
             including JavaScript, Java and Python.
           </p>
           <p>
-            I decided to take the plunge and leave my industrial design career
-            to study web development while looking after my young daughter.
+            After it was clear coding was something I wanted to do more
+            seriously I decided to take the plunge. I left my industrial design
+            career to study web development and pursue a new career while
+            looking after my young daughter.
           </p>
           <p>
-            I went gained several certficationas and built my knoladge bit by
-            bit. Working with mentors, local meetup groups and friends has
-            enabled me to get to where I am now.
+            Since that point I have been studying hard, gaining several
+            certifications and building my knowledge bit by bit. Working with
+            mentors, local meetup groups and friends has enabled me to get to
+            where I am now and I am excited to continue to grow and learn.
           </p>
         </div>
         <div className={classes.section_images}>
