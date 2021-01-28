9bdb33327b1684ce8bf92c4c14338bc340e95dd5
Jam52 <jamie.sajdak@gmail.com>
2021-01-28 13:28:39 -0500

    Update project images

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-  DotGroup,
-  Dot,
-} from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide, Dot } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import classes from './Carousel.module.scss';
 
