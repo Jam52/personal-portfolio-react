79ff214ff1a94602abf318e178e1a1c8b30a2c5c
Jam52 <jamie.sajdak@gmail.com>
2021-02-18 13:35:06 -0500

    Update header styles and icons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,7 @@ import classes from './Header.module.scss';
 const Header = (props) => {
   return (
     <div className={classes.container}>
+      <img src={props.icon} alt="" className={classes.icon} />
       <header className="container">
         <h1 className={classes.header}>
           {props.text}
--- src/Components/NavList/NavItem/NavItem.js
+++ src/Components/NavList/NavItem/NavItem.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import technicalIcon from './technicalSkills.svg';
-import aboutIcon from './aboutMe.svg';
-import contactIcon from './contactMe.svg';
-import pojectIcon from './projects.svg';
 import classes from './NavItem.module.scss';
 
 const NavItem = (props) => {
   let pathName = '/';
-  let icon;
+  let icon = process.env.PUBLIC_URL;
   let delayClass;
 
   switch (props.link) {
     case 'about me':
-      icon = aboutIcon;
+      icon = icon + '/images/aboutMe.svg';
       pathName = '/about';
       delayClass = classes.delayOne;
       break;
     case 'projects':
-      icon = pojectIcon;
+      icon = icon + '/images/projects.svg';
       pathName = '/projects';
       delayClass = classes.delayTwo;
       break;
     case 'skills':
-      icon = technicalIcon;
+      icon = icon + '/images/technicalSkills.svg';
       pathName = '/skills';
       delayClass = classes.delayThree;
       break;
     default:
-      icon = contactIcon;
+      icon = icon + '/images/contactMe.svg';
       pathName = '/contact';
       delayClass = classes.delayFour;
       break;
